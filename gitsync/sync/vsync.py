9d01e2c83f432d7d2831821fe48e65cf49776dca
Xinyu Ma <ma.xinyu.26a@kyoto-u.jp>
2020-05-25 20:46:23 -0700

    vsync add param

--- gitsync/sync/vsync.py
+++ gitsync/sync/vsync.py
@@ -1,14 +1,16 @@
 import typing
 from ndn.app import NDNApp
-from ndn.encoding import BinaryStr
+from ndn.encoding import BinaryStr, FormalName
 
 
 class VSync:
     OnUpdateFunc = typing.Callable[[BinaryStr], None]
 
-    def __init__(self, app: NDNApp, on_update):
+    def __init__(self, app: NDNApp, on_update, prefix: FormalName, interval: int):
         self.app = app
         self.on_update = on_update
+        self.prefix = prefix
+        self.interval = interval
 
     async def publish_update(self, content: BinaryStr):
         pass
